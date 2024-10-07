046b327d6dff3075319b7a3320f33d6a4466cc56
raj donda <155087919+rajdonda@users.noreply.github.com>
2024-10-08 00:23:38 +0530

    12

--- Pr 3. Event Handler/src/Product.jsx
+++ Pr 3. Event Handler/src/Product.jsx
@@ -4,13 +4,13 @@ import { category } from './Data'
 
 import './style.css'
 
-function Product({ product,filterProduct }) {
+function Product({ product, filterProduct }) {
 
 
 
     return (
         <div className='mt-5'>
-             <button onClick={ () => filterProduct("all") } className='btn btn-secondary'>All</button>
+            <button onClick={() => filterProduct("all")} className='btn btn-secondary'>All</button>
             {
                 category.map((cat) => {
                     return (
@@ -19,7 +19,7 @@ function Product({ product,filterProduct }) {
                 })
             }
 
-           
+
 
             <div className="container">
                 <div className="row mt-5">
@@ -28,10 +28,10 @@ function Product({ product,filterProduct }) {
                             return (
                                 <div key={p.id} className="col-lg-4 mb-4">
                                     <div className="card">
-                                        <img src={p.img} style={{height:"200px",objectFit:"contain"}} className="card-img-top" alt="..." />
+                                        <img src={p.img} style={{ height: "200px", objectFit: "contain" }} className="card-img-top" alt="..." />
                                         <div className="card-body">
                                             <h5 className="card-title">{p.name}</h5>
-                                            <p  className="card-text fs-5 fw-bold">RS :- {p.price}</p>
+                                            <p className="card-text fs-5 fw-bold">RS :- {p.price}</p>
                                             <a href="#" className="btn btn-success">Buy Now</a>
                                         </div>
                                     </div>
---
+++ pr-4 commant and review/src/App.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect, useRef } from "react"
+import './DynamicForm.css'
+
+function App() {
+
+  const [tableInput, SetInput] = useState([
+    { id: "", name: "", email: "", salary: "" }
+  ])
+
+  const inputRefs = useRef([]);
+
+  useEffect(() => {
+    const storedData = localStorage.getItem('tableInput');
+    if (storedData) {
+      SetInput(JSON.parse(storedData));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tableInput', JSON.stringify(tableInput));
+  }, [tableInput]);
+
+  const add = () => {
+    let newfield = { id: Math.floor(Math.random() * 1000), name: "", email: "", salary: "" }
+    SetInput([...tableInput, newfield]);
+  }
+  const remove = (id) => {
+    let updatedata = tableInput.filter((item) => item.id !== id); 
+    SetInput(updatedata);
+  }
+
+  const InputChange = (id, field, value, index) => {
+    const updatedtableInput = tableInput.map((item) =>
+      item.id === id ? { ...item, [field]: value } : item
+    );
+    SetInput(updatedtableInput);
+    // Focus on the next field when typing
+    if (index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      add();
+    }
+  };
+
+  return (
+    <div className="main" align="center">
+      <h1>Dynamic Table Management</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Full Name</th>
+            <th>Email Address</th>
+            <th>Salary</th>
+            <th><button className="btn1" onClick={() => add()}> + </button></th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            tableInput.map((val, index) => (
+              <tr key={val.id} align="center">
+                <td>
+                  <input type="text" value={val.name} onChange={(e) => InputChange(val.id, 'name', e.target.value, index)} placeholder="Enter Your Name" ref={el => inputRefs.current[index] = el} onKeyPress={handleKeyPress} />
+                </td>
+                <td>
+                  <input type="text" value={val.email} onChange={(e) => InputChange(val.id, 'email', e.target.value, index)} placeholder="Enter Your Email Id" onKeyPress={handleKeyPress} />
+                </td>
+                <td>
+                  <input type="text" value={val.salary} onChange={(e) => InputChange(val.id, 'salary', e.target.value, index)} placeholder="Enter Your Salary " onKeyPress={handleKeyPress} />
+                </td>
+                <td>
+                  <button className="btn2" onClick={() => remove(val.id)}>X</button>
+                </td>
+              </tr>
+            ))
+          }
+
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default App;
